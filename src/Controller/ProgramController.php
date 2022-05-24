d632f8a161dacee5c27342f5daf4da8079a90056
Mathias Bretonneau <Mathias.bretonneau@gmail.com>
2022-05-24 22:15:28 +0200

    Add categories for the quest 10

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -2,6 +2,8 @@
 // src/Controller/ProgramController.php
 namespace App\Controller;
 
+use App\Entity\Program;
+use App\Repository\ProgramRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,27 +12,44 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProgramController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(ProgramRepository $programRepository): Response
     {
+        $programs = $programRepository->findAll();
+
         return $this->render('program/index.html.twig', [
-            'website' => 'Wild Series',
+            'programs' => $programs
          ]);
     }
 
-    #[Route('/{id}', requirements: ['id' => '\d+'], methods: ['GET'], name: 'show')]
-    public function show(int $id): Response
+    #[Route('/show/{id}', requirements: ['id' => '\d+'], methods: ['GET'], name: 'show')]
+    public function show(int $id, ProgramRepository $programRepository): Response
     {
-        return $this->render('program/show.html.twig', ['id' => $id]);
+        $program = $programRepository->findOneBy(['id' => $id]);
+        // same as $program = $programRepository->find($id);
+
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program with id : '.$id.' found in program\'s table.'
+            );
+        }
+        return $this->render('program/show.html.twig', ['id' => $id, 'program' => $program,]);
     }
 }
 
+// #[Route('/program', name: 'program_')]
 // class ProgramController extends AbstractController
 // {
-//     #[Route('/program/', name: 'program_index')]
+//     #[Route('/', name: 'index')]
 //     public function index(): Response
 //     {
 //         return $this->render('program/index.html.twig', [
 //             'website' => 'Wild Series',
 //          ]);
+//     }
+
+//     #[Route('/{id}', requirements: ['id' => '\d+'], methods: ['GET'], name: 'show')]
+//     public function show(int $id): Response
+//     {
+//         return $this->render('program/show.html.twig', ['id' => $id]);
 //     }
 // }
--- src/DataFixtures/ProgramFixtures.php
+++ src/DataFixtures/ProgramFixtures.php
@@ -10,20 +10,20 @@ use Doctrine\Persistence\ObjectManager;
 class ProgramFixtures extends Fixture implements DependentFixtureInterface
 {
     public const SERIES = [
-        ['Title' => 'Resident evils', 'Synopsis' => 'Des zombies envahissent la terre', 'Category' => 'Action'],
-        ['Title' => 'Your Name', 'Synopsis' => 'Mitsuha, adolescente coincée dans une famille traditionnelle, rêve de quitter ses montagnes natales pour découvrir la vie trépidante de Tokyo.', 'Category' => 'Animation'],
-        ['Title' => 'Silent Hill: Revelation', 'Synopsis' => 'Arrivée à la veille de ses 18 ans, en proie à de terrifiants cauchemars, Heather doit faire face à la disparition soudaine de son père. Elle va découvrir qu\'elle n\'est pas celle qu\'elle croyait être.', 'Category' => 'Horreur'],
-        ['Title' => 'Teen wolf', 'Synopsis' => 'Transformé en loup-garou après avoir été mordu par un animal, un lycéen devient un sportif adulé et un bourreau des coeurs qui doit faire face à de nouveaux problèmes.', 'Category' => 'Aventure'],
-        ['Title' => 'stranger things', 'Synopsis' => 'En 1983, à Hawkins dans l\'Indiana, Will Byers disparaît de son domicile. Ses amis se lancent alors dans une recherche semée d\'embûches pour le retrouver. Pendant leur quête de réponses, les garçons rencontrent une étrange jeune fille en fuite.', 'Category' => 'Fantastique'],
+        ['title' => 'Resident evils', 'synopsis' => 'Des zombies envahissent la terre', 'category' => 'Action'],
+        ['title' => 'Your Name', 'synopsis' => 'Mitsuha, adolescente coincée dans une famille traditionnelle, rêve de quitter ses montagnes natales pour découvrir la vie trépidante de Tokyo.', 'category' => 'Animation'],
+        ['title' => 'Silent Hill: Revelation', 'synopsis' => 'Arrivée à la veille de ses 18 ans, en proie à de terrifiants cauchemars, Heather doit faire face à la disparition soudaine de son père. Elle va découvrir qu\'elle n\'est pas celle qu\'elle croyait être.', 'category' => 'Horreur'],
+        ['title' => 'Teen wolf', 'synopsis' => 'Transformé en loup-garou après avoir été mordu par un animal, un lycéen devient un sportif adulé et un bourreau des coeurs qui doit faire face à de nouveaux problèmes.', 'category' => 'Aventure'],
+        ['title' => 'stranger things', 'synopsis' => 'En 1983, à Hawkins dans l\'Indiana, Will Byers disparaît de son domicile. Ses amis se lancent alors dans une recherche semée d\'embûches pour le retrouver. Pendant leur quête de réponses, les garçons rencontrent une étrange jeune fille en fuite.', 'category' => 'Fantastique'],
     ];
 
     public function load(ObjectManager $manager)
     {
         foreach (self::SERIES as $serie) {
             $program = new Program();
-            $program->setTitle($serie['Title']);
-            $program->setSynopsis($serie['Synopsis']);
-            $program->setCategory($this->getReference('category_' . $serie['Category']));
+            $program->setTitle($serie['title']);
+            $program->setSynopsis($serie['synopsis']);
+            $program->setCategory($this->getReference('category_' . $serie['category']));
             $manager->persist($program);
         }
         $manager->flush();
